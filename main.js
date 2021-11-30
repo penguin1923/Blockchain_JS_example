741385624b10d0a41ef96c636166142e208d1450
Don <64542224+penguin1923@users.noreply.github.com>
2021-11-30 17:33:03 -0500

    finished block class moving on to blockchain class

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const SHA256 = require("crypto-js/sha256");
+
 class Block
 {
     constructor(index,timestamp,data,previousHash ='')
@@ -6,11 +8,24 @@ class Block
         this.timestamp=timestamp;
         this.data=data;
         this.previousHash=previousHash;
-        this.hash = '';
+        this.hash = this.calculateHash;
     }
 
     calculateHash()
     {
-        
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class Blockchain
+{
+    constructor()
+    {
+        this.chain=[];
+    }
+
+    createGenesisBlock()
+    {
+        return new Block(0,"11/30/2021","starting block","8675309");
     }
 }
