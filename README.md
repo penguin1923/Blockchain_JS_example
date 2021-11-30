889bdfe338f490c70604390452ed3354e1af4a7b
Don <64542224+penguin1923@users.noreply.github.com>
2021-11-30 17:15:18 -0500

    added block class

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Blockchain_JS_example
  Uses a tutorial to create a basic blockchain example
+
+ Tutorial: https://www.youtube.com/watch?v=zVqczFZr124
---
+++ main.js
@@ -0,0 +1,16 @@
+class Block
+{
+    constructor(index,timestamp,data,previousHash ='')
+    {
+        this.index=index;
+        this.timestamp=timestamp;
+        this.data=data;
+        this.previousHash=previousHash;
+        this.hash = '';
+    }
+
+    calculateHash()
+    {
+        
+    }
+}
