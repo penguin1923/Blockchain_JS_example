04ff92948d9848064e86c26d27ea6ad3c66c0612
Don <64542224+penguin1923@users.noreply.github.com>
2021-11-30 20:21:49 -0500

    added a check to make sure chain is valid

--- main.js
+++ main.js
@@ -40,6 +40,26 @@ class Blockchain
         newBlock.hash=newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+    //integrity verification
+    isChainValid()
+    {
+        for(let i=1;i<this.chain.length;i++)
+        {
+            const currentBlock=this.chain[i];
+            const previousBlock=this.chain[i-1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash())
+            {
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 let jennyFromTheBlock = new Blockchain();
@@ -47,5 +67,9 @@ let jennyFromTheBlock = new Blockchain();
 //created a couple of block for the purpose of seeing the chain play out
 jennyFromTheBlock.addBlock(new Block(1,"12/01/2021",{Jenny: "i got your number"}));
 jennyFromTheBlock.addBlock(new Block(2,"12/02/2021",{Jenny:"don't change your number"}));
+
 //node main.js
-console.log(JSON.stringify(jennyFromTheBlock,null,4));
+//console.log(JSON.stringify(jennyFromTheBlock,null,4));
+
+//test validity
+console.log('Is blockchain valid?'+ jennyFromTheBlock.isChainValid());
